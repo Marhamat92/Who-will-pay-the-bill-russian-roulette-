01b0649423dbf214ab35d245cdabc3be99c1247d
Marhamat <marhamatbagirov@gmail.com>
2021-01-05 21:07:20 +0000

    an easy way of choosing

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import random
 
 names_string=input("Please type friends names seperated by comma: ")
 names=names_string.split(", ")
-number_names=len(names)
 
-random_choice=random.randint(0,number_names-1)
-who_will_pay=names[random_choice]
+#we can also use this way to understand deeply how it was created
+#number_names=len(names)
+#random_choice=random.randint(0,number_names-1)
+#who_will_pay=names[random_choice]
+
+who_will_pay=random.choice(names)
 print(who_will_pay + " is gonna pay the meal today")
